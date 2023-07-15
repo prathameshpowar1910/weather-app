8521fef38dc2e99b131155fc02fb19300248548d
Prathamesh Powar <ppowar1910@gmail.com>
2023-07-15 16:33:17 +0530

    made change to allow deployment on render

--- app.js
+++ app.js
@@ -40,6 +40,6 @@ app.post('/',(req,res)=>{
 })
 
 
-app.listen( env.process.PORT || 3000, () => {
+app.listen( process.env.PORT || 3000, () => {
   console.log("Server is running on port 3000");
 });
